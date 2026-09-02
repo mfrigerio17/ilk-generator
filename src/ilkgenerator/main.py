1e1a472c1ffa8b1f235a42d893ec7e8339f48f53
Marco Frigerio <3646506+mfrigerio17@users.noreply.github.com>
2026-09-02 11:56:06 +0200

    Consolidate the main module
    Use 'pathlib' instead of 'os', fix minor details,
    separate command line argument parsing and actual
    generation of the ILK files.

--- src/ilkgenerator/main.py
+++ src/ilkgenerator/main.py
@@ -1,4 +1,4 @@
-import logging, os, argparse, yaml
+import logging, pathlib, argparse, yaml
 
 import robmodel
 import rmt.rmt as rmtool
@@ -11,46 +11,7 @@ log = logging.getLogger(__name__)
 
 default_outdir = "/tmp/ilk"
 
-def main():
-    formatter = logging.Formatter('%(levelname)s : %(message)s')
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    log.setLevel(logging.WARN)
-    log.addHandler(handler)
-
-    argparser = argparse.ArgumentParser(prog="ilkgen", description='Generate ILK solver models')
-
-    rmtool.setRobotArgs(argparser)
-
-    argparser.add_argument('-q', '--query', metavar='QUERY', dest='query',
-            help='the YAML file containing a query (defaults to a random FK solver)')
-    argparser.add_argument('-o', '--output-dir', metavar='ODIR', dest='odir',
-            default = default_outdir,
-            help='the directory where to put the generated files (defaults to ' + default_outdir + ')')
-
-    args = argparser.parse_args()
-
-    connectivity, tree, robotframes, geometrymodel, inertia, params = rmtool.getmodels(args.robot, args.params)[0:6]
-    robotmodel = tree # this is the model composed of connectivity plus numbering scheme
-    rmtool._resolve_parameters(geometrymodel.posesModel.poses, params)
-
-    if args.query :
-        istream = open(args.query)
-        userq   = query.queryFromYAML( istream )
-        istream.close()
-    else :
-        userq = query.defaultQuery(robotmodel)
-
-    try:
-        qparser = query.QueryParser(robotmodel, robotframes, None)
-        sweepingsolvers, iksolvers = qparser.validate(userq)
-    except Exception as e:
-        log.error("Parsing exception: %s", e)
-        return -1
-
-    if not os.path.exists(args.odir) :
-        os.makedirs(args.odir)
-
+def generateILKFiles(outdirpath, geometrymodel, sweepingsolvers, iksolvers):
     ikSolverModels = []
     for sspecs in iksolvers :
         solver = solvermodel.IKSolverModel(sspecs)
@@ -71,18 +32,59 @@ def main():
         solver = solvermodel.FKSolverModel(sspecs)
         gen = generator.SweepingSolverGenerator(solver)
         lua = gen.lua()
-        ostream = open(args.odir + "/" + solver.name + ".ilk", mode='w')
-        ostream.write(lua)
-        ostream.close()
+        with open(outdirpath / (solver.name + ".ilk"), mode='w') as ostream:
+            ostream.write(lua)
 
     for solver in ikSolverModels :
         gen = generator.IKGenerator(solver)
         lua = gen.lua()
-        ostream = open(args.odir + "/" + solver.name + ".ilk", mode='w')
-        ostream.write(lua)
-        ostream.close()
+        with open(outdirpath / (solver.name + ".ilk"), mode='w') as ostream:
+            ostream.write(lua)
 
     kk = robotconstants.asLuaTable(geometrymodel)
-    ostream = open(args.odir + "/model-constants.lua", mode='w')
-    ostream.write(kk)
-    ostream.close()
+    with open(outdirpath / "model-constants.lua", mode='w') as ostream:
+        ostream.write(kk)
+
+
+def main():
+    formatter = logging.Formatter('%(levelname)s : %(message)s')
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    log.setLevel(logging.WARN)
+    log.addHandler(handler)
+
+    argparser = argparse.ArgumentParser(prog="ilkgen", description='Generate ILK solver models')
+
+    rmtool.setRobotArgs(argparser)
+
+    argparser.add_argument('-q', '--query', metavar='QUERY', dest='query',
+            help='the YAML file containing a query (defaults to a random FK solver)')
+    argparser.add_argument('-o', '--output-dir', metavar='ODIR', dest='odir',
+            default = default_outdir,
+            help=f"the directory where to put the generated files (defaults to '{default_outdir}')")
+
+    args = argparser.parse_args()
+
+    _, robotmodel, robotframes, geometrymodel, _, params = rmtool.getmodels(args.robot, args.params)[0:6]
+    ## 'robotmodel' is the model composed of connectivity plus numbering scheme
+    rmtool._resolve_parameters(geometrymodel.posesModel.poses, params)
+
+    if args.query :
+        with open(args.query) as istream:
+            userq = query.queryFromYAML( istream )
+    else :
+        userq = query.defaultQuery(robotmodel)
+
+    try:
+        qparser = query.QueryParser(robotmodel, robotframes, None)
+        sweepingsolvers, iksolvers = qparser.validate(userq)
+    except Exception as e:
+        log.error("Parsing exception: %s", e)
+        return -1
+
+    outdir = pathlib.Path(args.odir)
+    if not outdir.exists():
+        outdir.mkdir(parents=True)
+
+    generateILKFiles(outdir, geometrymodel, sweepingsolvers, iksolvers)
+
